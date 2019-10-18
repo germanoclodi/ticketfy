da2b6a705eded8dba784a52d8a02da9503c20303
Germano Lodi <lodi@MacBook-Pro-de-ateliware.local>
2019-10-18 11:08:29 -0300

    Heroku configs_prod.exs error

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :ticket_sender, TicketSenderWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "ticketfy-sender.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Configure your database
@@ -71,4 +71,5 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-# import_config "prod.secret.exs"
+# 
+import_config "prod.secret.exs"

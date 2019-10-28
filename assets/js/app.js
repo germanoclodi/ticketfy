e0b2b2b370dfbb8eb3c6d00cb6a0c63186df66ce
lodi <germano.c.lodi@gmail.com>
2019-10-28 19:56:31 -0300

    Alert fadeout

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,10 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+
+window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+    });
+}, 2000);
